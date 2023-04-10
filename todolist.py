93c6de4ea498fe6f8ae7077dd48827b6e0a2f28e
aajbaker-brandeis <109557665+aajbaker-brandeis@users.noreply.github.com>
2023-04-10 14:31:55 -0400

    New version of Hickey project. Prepared for teamwork.

--- todolist.py
+++ todolist.py
@@ -1,121 +1,47 @@
-#! /opt/miniconda3/bin/python3
-'''
-todo2 is an app that maintains a todo list
-just as with the todo code in this folder.
+import sqlite3
+import os
 
-but it also uses an Object Relational Mapping (ORM)
-to abstract out the database operations from the
-UI/UX code.
+def toDict(t):
+    ''' t is a tuple (rowid,title, desc,completed)'''
+    print('t='+str(t))
+    todo = {'rowid':t[0], 'title':t[1], 'desc':t[2], 'completed':t[3]}
+    return todo
 
-The ORM, TodoList, will map SQL rows with the schema
-    (rowid,title,desc,completed)
-to Python Dictionaries as follows:
-
-(5,'commute','drive to work',false) <-->
-
-{rowid:5,
- title:'commute',
- desc:'drive to work',
- completed:false)
- }
-
-In place of SQL queries, we will have method calls.
-
-This app will store the data in a SQLite database ~/todo.db
-
-Recall that sys.argv is a list of strings capturing the
-command line invocation of this program
-sys.argv[0] is the name of the script invoked from the shell
-sys.argv[1:] is the rest of the arguments (after arg expansion!)
-
-Note the actual implementation of the ORM is hidden and so it 
-could be replaced with PostgreSQL or Pandas or straight python lists
-
-'''
-
-from todolist import TodoList
-import sys
-
-
-# here are some helper functions ...
-
-def print_usage():
-    ''' print an explanation of how to use this command '''
-    print('''usage:
-            todo show
-            todo showall
-            todo showcomplete
-            todo add name description
-            todo complete item_id
-            todo delete item_id
-            '''
-            )
-
-def print_todos(todos):
-    ''' print the todo items '''
-    if len(todos)==0:
-        print('no tasks to print')
-        return
-    print('\n')
-    print("%-10s %-10s %-30s %-10s"%('item #','title','desc','completed'))
-    print('-'*40)
-    for item in todos:
-        values = tuple(item.values()) #(rowid,title,desc,completed)
-        print("%-10s %-10s %-30s %2d"%values)
-
-def process_args(arglist):
-    ''' examine args and make appropriate calls to TodoList'''
-    todolist = TodoList()
-    if arglist==[]:
-        print_usage()
-    elif arglist[0]=="show":
-        print_todos(todolist.selectActive())
-    elif arglist[0]=="showall":
-        print_todos(todos = todolist.selectAll())
-    elif arglist[0]=="showcomplete":
-        print_todos(todolist.selectCompleted())
-    elif arglist[0]=='add':
-        if len(arglist)!=3:
-            print_usage()
-        else:
-            todo = {'title':arglist[1],'desc':arglist[2],'completed':0}
-            todolist.add(todo)
-    elif arglist[0]=='complete':
-        if len(arglist)!= 2:
-            print_usage()
-        else:
-            todolist.setComplete(arglist[1])
-    elif arglist[0]=='delete':
-        if len(arglist)!= 2:
-            print_usage()
-        else:
-            todolist.delete(arglist[1])
-    else:
-        print(arglist,"is not implemented")
-        print_usage()
-
-
-def toplevel():
-    ''' read the command args and process them'''
-    if len(sys.argv)==1:
-        # they didn't pass any arguments, 
-        # so prompt for them in a loop
-        print_usage()
-        args = []
-        while args!=['']:
-            args = input("command> ").split(' ')
-            if args[0]=='add':
-                # join everyting after the name as a string
-                args = ['add',args[1]," ".join(args[2:])]
-            process_args(args)
-            print('-'*40+'\n'*3)
-    else:
-        # read the args and process them
-        args = sys.argv[1:]
-        process_args(args)
-        print('-'*40+'\n'*3)
-
+class TodoList():
+    def __init__(self):
+        self.runQuery('''CREATE TABLE IF NOT EXISTS todo
+                    (title text, desc text, completed int)''',())
     
+    def selectActive(self):
+        ''' return all of the uncompleted tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo where completed=0",())
 
-toplevel()
+    def selectAll(self):
+        ''' return all of the tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo",())
 
+    def selectCompleted(self):
+        ''' return all of the completed tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo where completed=1",())
+
+    def add(self,item):
+        ''' create a todo item and add it to the todo table '''
+        return self.runQuery("INSERT INTO todo VALUES(?,?,?)",(item['title'],item['desc'],item['completed']))
+
+    def delete(self,rowid):
+        ''' delete a todo item '''
+        return self.runQuery("DELETE FROM todo WHERE rowid=(?)",(rowid,))
+
+    def setComplete(self,rowid):
+        ''' mark a todo item as completed '''
+        return self.runQuery("UPDATE todo SET completed=1 WHERE rowid=(?)",(rowid,))
+
+    def runQuery(self,query,tuple):
+        ''' return all of the uncompleted tasks as a list of dicts.'''
+        con= sqlite3.connect(os.getenv('USERPROFILE')+'/todo.db')
+        cur = con.cursor() 
+        cur.execute(query,tuple)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return [toDict(t) for t in tuples]
